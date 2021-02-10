698354b361be93aee491e0b81f72bd0f5bba2a09
Aitor Ventura <43577217+Aitor-Ventura@users.noreply.github.com>
2021-02-10 22:10:53 +0000

    Add files via upload

---
+++ Ejercicio 16 (MayorMenosParametros).java
@@ -0,0 +1,11 @@
+// El método max devuelve el mayor de los dos 
+// valores pasados por parámetros, value1 y value2.
+public class Excercise {
+    public static double max(double value1, double value2) {
+        if (value1 > value2){
+            return value1;
+        } else {
+            return value2;
+        }
+    }
+}
